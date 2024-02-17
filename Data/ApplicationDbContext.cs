f16ce18b34e6ad5cf3c13a4aa28077aa345a2b44
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-02-17 23:49:41 +0700

    Đăng ký user account by API Identity JWT

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,18 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //MĐ tạo và đưa dữ liệu về các quyền (roles) vào cơ sở dữ liệu bằng cách sử dụng Entity Framework Core.
+            List<IdentityRole> roles = new List<IdentityRole>()
+            {
+                new IdentityRole() {Name = "Admin", NormalizedName="ADMIN"},
+                new IdentityRole() {Name = "User", NormalizedName="USER"}
+            };
+            //MĐ thêm các quyền đã khởi tạo trong roles vào bảng IdentityRole in csdl
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
